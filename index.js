fe0b91deeb2f1abd213eb8b5581ca981b4c425f7
Mohnish Thallavajhula <i@mohni.sh>
2017-02-02 17:42:54 -0800

    add basic implementation

---
+++ index.js
@@ -0,0 +1,11 @@
+export default function wrap (fn, ...args) {
+  return new Promise((resolve, reject) => {
+    fn.apply(this, args, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
---
+++ test/index.test.js
@@ -0,0 +1,26 @@
+import wrap from '../';
+import { expect } from 'chai';
+
+describe('wrap', () => {
+  xit('wraps the given fn in a Promise', () => {
+    expect(1).to.equal(1);
+  });
+
+  describe('with no args', () => {
+    xit('wraps the fn with no args', () => {
+      expect(1).to.equal(1);
+    });
+  });
+
+  describe('with one arg', () => {
+    xit('wraps the fn with one arg', () => {
+      expect(1).to.equal(1);
+    });
+  });
+
+  describe('with more than one arg', () => {
+    xit('wraps the fn with multiple args', () => {
+      expect(1).to.equal(1);
+    });
+  });
+});
