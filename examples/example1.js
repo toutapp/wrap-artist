2bf4d5436090e0b95d6e69662f27495511b111f6
Mohnish Thallavajhula <i@mohni.sh>
2017-02-02 17:52:41 -0800

    add working examples

--- examples/example1.js
+++ examples/example1.js
@@ -1,1 +1,13 @@
-// TODO: (MT) fill this with an example
+import wrap from '../';
+
+async function sayHiAfterASecond() {
+  console.log('about to say hi');
+  await wrap(setTimeout, () => { console.log('Hi') }, 1e3);
+  console.log('i just said hi');
+}
+
+sayHiAfterASecond();
+
+// => about to say hi
+// => Hi
+// => i just said hi
