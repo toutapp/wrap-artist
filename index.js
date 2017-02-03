3523992e04b07964d6b5b8c01f2f0ef01adb93d1
Mohnish Thallavajhula <i@mohni.sh>
2017-02-03 10:32:50 -0800

    update var name

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 export default function wrap(fn, ...args) {
   return new Promise((resolve, reject) => {
-    fn.apply(this, args, (err, res) => {
+    fn.apply(this, args, (err, result) => {
       if (err) {
         reject(err);
       } else {
-        resolve(res);
+        resolve(result);
       }
     });
   });
