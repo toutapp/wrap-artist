e7f517cfd64108126b27b57edf07227c5a8ac88a
Mohnish Thallavajhula <i@mohni.sh>
2017-02-03 12:50:45 -0800

    include build

---
+++ build/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = wrap;
+function wrap(fn, ...args) {
+  if (typeof fn != 'function') throw new Error('wrap(...) requires a function to be passed in.');
+
+  return new Promise((resolve, reject) => {
+    fn.call(this, ...args, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
