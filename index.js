06ff17635c079a0cbb2cbd42c395e1930a7a2f7d
Mohnish Thallavajhula <i@mohni.sh>
2017-02-03 12:34:15 -0800

    call the callback

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 export default function wrap(fn, ...args) {
+  if (typeof fn != 'function') throw new Error('wrap(...) requires a function to be passed in.');
+
   return new Promise((resolve, reject) => {
-    fn.apply(this, args, (err, result) => {
+    fn.call(this, ...args, (err, result) => {
       if (err) {
         reject(err);
       } else {
